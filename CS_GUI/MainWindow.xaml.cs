0f2c2d240cad4c34218d6f77a17a46140c3df9b4
LukeWilsonUEA <luke.wilson@uea.ac.uk>
2025-10-26 22:30:13 +0000

    Refactor project structure, implement GUI1 task.

---
+++ CS_GUI/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                errorBox.Text = ""; // clear error box text
+                outputBox.Text = ""; // clear output box text
+
+                string inputText = inputBox.Text;
+                string interpreterReturn = Simple_Interpreter.GUIInterpret.interpret(inputText);
+                outputBox.Text = interpreterReturn;
+            }
+            catch (Exception ex)
+            {
+                errorBox.Text = ex.Message;
+            }
+
+        }
+    }
+}
---
+++ CS_GUI/TopMenu.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_GUI
+{
+    /// <summary>
+    /// Interaction logic for TopMenu.xaml
+    /// </summary>
+    public partial class TopMenu : UserControl
+    {
+        public TopMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void SyntaxHelpMenuClicked(object sender, RoutedEventArgs e)
+        {
+            SyntaxHelpWindow helpWindow = new SyntaxHelpWindow();
+            helpWindow.Show();
+        }
+    }
+}
